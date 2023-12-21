924d153022c7a99ac64d1c19aa77f542c4f45458
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-12-21 16:39:58 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import ExpenseItem from './components/ExpenseItem';
 
+const expenses = [
+  { id: 1, expenseName: 'Food', amount: 'Rs 10', locationOfExpenditure: 'Grocery Store' },
+  { id: 2, expenseName: 'Petrol', amount: 'Rs 100', locationOfExpenditure: 'Gas Station' },
+  { id: 3, expenseName: 'Movies', amount: 'Rs 200', locationOfExpenditure: 'Cinema' },
+];
+
+const expenseItems = [];
+for (let i = 0; i < expenses.length; i++) {
+  const expense = expenses[i];
+  expenseItems.push(
+    <ExpenseItem
+      key={expense.id}
+      expenseName={expense.expenseName}
+      amount={expense.amount}
+      locationOfExpenditure={expense.locationOfExpenditure}
+    />
+  );
+}
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+    <div className="expense-list">
+      <h2>Expense Items</h2>
+      <div>
+        {expenseItems}
+      </div>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import './ExpenseItem.css';
+   
 
-function ExpenseItem() {
-  const LocationOfExpenditure = "Grocery Store"; 
+function ExpenseItem(props) {
 
   return (
-    <div className="expense-list">
-      <h2>Expense Items</h2>
-      <ul>
-        <li className="expense-list-item">
-          <h2>Food</h2>
-          <p className="expense-location">{LocationOfExpenditure}</p>
-          <div className="expense-list-item__price">Rs 10</div>
-        </li>
-        <li className="expense-list-item">
-          <h2>Petrol</h2>
-          <p className="expense-location">{LocationOfExpenditure}</p>
-          <div className="expense-list-item__price">Rs 100</div>
-        </li>
-        <li className="expense-list-item">
-          <h2>Movies</h2>
-          <p className="expense-location">{LocationOfExpenditure}</p>
-          <div className="expense-list-item__price">Rs 200</div>
-        </li>
-      </ul>
-    </div>
+    <li className="expense-list-item">
+      <h2>{props.expenseName}</h2>
+      <p className="expense-location">{props.locationOfExpenditure}</p>
+      <div className="expense-list-item__price">{props.amount}</div>
+    </li>
   );
 }
 
