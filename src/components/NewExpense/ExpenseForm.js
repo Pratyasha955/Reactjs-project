187e2b8052b4ed04b284eb33f44bc07f718c1d4f
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-12-23 17:15:58 +0530

    state inside the new Component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+  
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
-      };
-    
-      const amountChangeHandler = (event) => {
-        console.log(event.target.value);
-      };
-    
-      const dateChangeHandler = (event) => {
-        console.log(event.target.value);
-      };
-    
+      setEnteredTitle(event.target.value);
+    };
+  
+    const amountChangeHandler = (event) => {
+      setEnteredAmount(event.target.value);
+    };
+  
+    const dateChangeHandler = (event) => {
+      setEnteredDate(event.target.value);
+    };
+  
       return (
         <form>
           <div className='new-expense__controls'>
