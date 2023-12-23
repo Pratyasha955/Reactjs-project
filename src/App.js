ed6e345454fbcffd0c6aac05317ab31bff76d76d
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-12-23 19:05:32 +0530

    Chart

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const DUMMY_EXPENSES = [
-    { id: 1, date: new Date(2023, 11, 18), expenseName: 'Food', amount: '10', location: 'Grocery Store' },
-    { id: 2, date: new Date(2020, 11, 18), expenseName: 'Petrol', amount: '500', location: 'Gas Station' },
-    { id: 3, date: new Date(2022, 11, 18), expenseName: 'Movies', amount: '200', location: 'Cinema' },
-    { id: 4, date: new Date(2023, 5, 12), expenseName: 'Electronics', amount: '800', location: 'Tech Store' },
-    { id: 5, date: new Date(2022, 8, 28), expenseName: 'Clothing', amount: '150', location: 'Fashion Mall' },
-    { id: 6, date: new Date(2021, 2, 5), expenseName: 'Books', amount: '50', location: 'Bookstore' },
-    { id: 7, date: new Date(2022, 7, 15), expenseName: 'Dining Out', amount: '80', location: 'Restaurant' },
-    { id: 8, date: new Date(2023, 10, 20), expenseName: 'Healthcare', amount: '300', location: 'Pharmacy' },
-    { id: 9, date: new Date(2023, 3, 9), expenseName: 'Home Supplies', amount: '120', location: 'Supermarket' },
-    { id: 10, date: new Date(2023, 6, 17), expenseName: 'Travel', amount: '350', location: 'Travel Agency' },
-    { id: 11, date: new Date(2022, 1, 25), expenseName: 'Gym Membership', amount: '75', location: 'Fitness Center' },
-    { id: 12, date: new Date(2021, 4, 3), expenseName: 'Car Maintenance', amount: '200', location: 'Auto Repair Shop' },
-    { id: 13, date: new Date(2020, 9, 10), expenseName: 'Education', amount: '250', location: 'School Supplies Store' },
+    { id: 1, date: new Date(2023, 11, 18), title: 'Food', amount: '10', location: 'Grocery Store' },
+    { id: 2, date: new Date(2020, 11, 18), title: 'Petrol', amount: '500', location: 'Gas Station' },
+    { id: 3, date: new Date(2022, 11, 18), title: 'Movies', amount: '200', location: 'Cinema' },
+    { id: 4, date: new Date(2023, 5, 12), title: 'Electronics', amount: '800', location: 'Tech Store' },
+    { id: 5, date: new Date(2022, 8, 28), title: 'Clothing', amount: '150', location: 'Fashion Mall' },
+    { id: 6, date: new Date(2021, 2, 5), title: 'Books', amount: '50', location: 'Bookstore' },
+    { id: 7, date: new Date(2022, 7, 15), title: 'Dining Out', amount: '80', location: 'Restaurant' },
+    { id: 8, date: new Date(2023, 10, 20), title: 'Healthcare', amount: '300', location: 'Pharmacy' },
+    { id: 9, date: new Date(2023, 3, 9), title: 'Home Supplies', amount: '120', location: 'Supermarket' },
+    { id: 10, date: new Date(2023, 6, 17), title: 'Travel', amount: '350', location: 'Travel Agency' },
+    { id: 11, date: new Date(2022, 1, 25), title: 'Gym Membership', amount: '75', location: 'Fitness Center' },
+    { id: 12, date: new Date(2021, 4, 3), title: 'Car Maintenance', amount: '200', location: 'Auto Repair Shop' },
+    { id: 13, date: new Date(2020, 9, 10), title: 'Education', amount: '250', location: 'School Supplies Store' },
   ];
   // const expenseItems = [];
   // for (let i = 0; i < expenses.length; i++) {
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,7 +6,7 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.expenseName);
+  const [title, setTitle] = useState(props.title);
   const [expense, setExpense] = useState(props.amount);
   const deleteExpenseHandler = () => {
     console.log('Deleted')
