644e01da35eb7421fb4723e4c41e9f9fd0d1e823
Pratyasha955 <pratyashasatapathy79@gmail.com>
2023-12-23 13:50:43 +0530

    useState and new button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
@@ -6,23 +6,29 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.expenseName);
+  const [expense, setExpense] = useState(props.amount);
   const deleteExpenseHandler = () => {
     console.log('Deleted')
   };
-
-  const clickHandler = () => {
-    console.log('updated')
+  const changeExpenseHandler = () => {
+    setExpense('100')
+    console.log(expense)
   };
-
+  const clickHandler = () => {
+    setTitle('Updated')
+    console.log(title)
+  };
   return (
     <Card className="expense-list-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
-          amount={props.amount}
-          location={props.location}
-          title={props.expenseName}
-        />
-        <button onClick={clickHandler}>Change Title</button>
+        amount={expense}
+        location={props.location}
+        title={title}
+      />
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={changeExpenseHandler}>Change Expense</button>
       <button onClick={deleteExpenseHandler}>Delete Expense</button>
     </Card>
   );
